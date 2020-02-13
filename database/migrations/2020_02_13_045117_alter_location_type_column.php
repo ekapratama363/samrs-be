705c0411bd14c500ea29a52b046ec7ff7b8b2213
alfredo_ruben <alpredo.tampubolon@gmail.com>
2020-02-13 12:20:35 +0700

    Storage, Plant, Location dan Location Type

---
+++ database/migrations/2020_02_13_045117_alter_location_type_column.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterLocationTypeColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('location_types', function (Blueprint $table) {
+            $table->string('code')->unique()->nullable();
+            $table->string('description')->nullable();
+            $table->string('name')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('location_types', function (Blueprint $table) {
+            $table->dropColumn('code');
+            $table->dropColumn('description');
+            $table->string('name')->change();
+        });
+    }
+}
