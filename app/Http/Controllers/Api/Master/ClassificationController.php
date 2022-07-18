982f5036d0165f012251727ff3504111cf38cba0
ekapratama363 <ekapratama363@gmail.com>
2022-07-18 08:28:31 +0700

    disable decimal on classification parameter update

--- app/Http/Controllers/Api/Master/ClassificationController.php
+++ app/Http/Controllers/Api/Master/ClassificationController.php
@@ -447,7 +447,7 @@ class ClassificationController extends Controller
             // numeric
             $this->validate(request(), [
                 'length' => 'required|numeric',
-                'decimal' => 'required|numeric'
+                // 'decimal' => 'required|numeric'
             ]);
         } else if (request()->input('type') == 5) {
             // list
